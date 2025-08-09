1f348794c92038897d6eeae916afd309403d3f6b
Aayush Kumar Singh <aayu.kumar2004@gmail.com>
2025-08-09 21:50:08 +0530

    Fix Navbar path issue for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
 import About from './components/About/About';
 import BlurBlob from './components/BlurBlob';
 import Contact from './components/Contact/Contact';
 import Education from './components/Education/Education';
 import Experience from './components/Experience/Experience';
 import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
 import Project from './components/Project/Project';
 import Skills from './components/Skills/Skills';
 
